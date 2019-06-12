0fc27642dc8d8bae6d343cdbe6184b9d5d9e9962
Gabriel Abarca Saavedra <gabriel.abarca.saavedra@gmail.com>
2019-06-11 23:11:15 -0400

    mayuscula

--- src/api/children/model.js
+++ src/api/children/model.js
@@ -165,7 +165,7 @@ export const parvulo = dato => {
   return new Promise((resolve, reject) => {
     const connection = mysql.createConnection(mysqlConfig)
     connection.connect()
-    var sql = 'select P.*,A.nombre as apoderado1 from parvulo as P inner join apoparvulo as ap ON p.id=ap.id_parvulo INNER JOIN apoderado AS a ON ap.id_apoderado=a.id WHERE p.id=?'
+    var sql = 'SELECT P.*,A.nombre as apoderado1 from parvulo as P inner join apoparvulo as AP ON P.id=AP.id_parvulo INNER JOIN apoderado AS A ON AP.id_apoderado=A.id WHERE P.id=?'
     connection.query(sql, dato.id, (error, results, field) => {
       if (error) console.log("soy un error", error)
       else resolve(results)
